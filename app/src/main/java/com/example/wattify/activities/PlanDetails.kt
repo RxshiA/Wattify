20fb29453dc9db7ce8e3f24db671a0af02b9dde7
Yasanjith Rajapathirane <125336683+YasanjithR@users.noreply.github.com>
2023-05-14 02:33:33 +0530

    Quesry dvices working

--- app/src/main/java/com/example/wattify/activities/PlanDetails.kt
+++ app/src/main/java/com/example/wattify/activities/PlanDetails.kt
@@ -3,15 +3,19 @@ package com.example.wattify.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wattify.R
+import com.example.wattify.adapters.DevAdapter
 import com.example.wattify.models.DeviceModel
 import com.example.wattify.models.PlanModel
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class PlanDetails : AppCompatActivity() {
 
@@ -20,6 +24,10 @@ class PlanDetails : AppCompatActivity() {
     private lateinit var tvPlanName:TextView
     private lateinit var btnUpdate:Button
     private lateinit var btnDelete:Button
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var devList: ArrayList<DeviceModel>
+    private lateinit var devRecyclerView: RecyclerView
+    private lateinit var tvLoadingData: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +49,19 @@ class PlanDetails : AppCompatActivity() {
                 intent.getStringExtra("planId").toString()
             )
         }
+
+
+        devRecyclerView = findViewById(R.id.rvDev)
+        devRecyclerView.layoutManager = LinearLayoutManager(this)
+        devRecyclerView.setHasFixedSize(true)
+        tvLoadingData = findViewById(R.id.tvLoadingData)
+        devList = arrayListOf<DeviceModel>()
+
+        getDevicesData()
+
+
+
+
 
     }
 
@@ -126,5 +147,58 @@ class PlanDetails : AppCompatActivity() {
             Toast.makeText(this, "Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
         }
     }
+
+    private fun getDevicesData() {
+
+        devRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+        dbRef = FirebaseDatabase.getInstance("https://wattify-ce140-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Devices")
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                devList.clear()
+                if (snapshot.exists()){
+                    for (devSnap in snapshot.children){
+                        val devData = devSnap.getValue(DeviceModel::class.java)
+                        devList.add(devData!!)
+
+                    }
+
+                    val mAdapter = DevAdapter(devList)
+                    devRecyclerView.adapter = mAdapter
+
+                    mAdapter.setOnItemClickListener(object : DevAdapter.onItemClickListener{
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(this@PlanDetails, DeviceDetailsActivity::class.java)
+
+                            //put extras
+                            intent.putExtra("devId", devList[position].devId)
+                            intent.putExtra("devName", devList[position].devName)
+                            intent.putExtra("devWatts", devList[position].devWatts)
+                            intent.putExtra("devType", devList[position].devType)
+                            startActivity(intent)
+                        }
+
+
+
+                    })
+
+                    devRecyclerView.visibility = View.VISIBLE
+                    tvLoadingData.visibility = View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+
+
+
 }
 
